bd00e7793f79ad5dd1d2b72e76a98fb663ba15fa
novotnyr.05 <novotnyr.05@A222-16>
2022-03-31 11:32:26 +0200

    p07 rozd

---
+++ p07/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] pole = new string[23] {  "._", "_...", "_._.", "_..", ".", ".._.", "__.", "....", "..", ".___", "_._", "._..", "__", " _.", "___", ".__.", "__._", "._.", "...", "_", ".._", "..._", ".__", "_.._", "_.__", "__.." };
+            string text = textBox1.Text;
+            string mor = "";
+            foreach (char s in text)
+            {
+
+            }
+        }
+    }
+}
