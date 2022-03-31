5735579ef435b548f3a48f904304334b061665b9
novotnyr.05 <novotnyr.05@A222-16>
2022-03-31 11:06:39 +0200

    p02

--- p02/Form1.cs
+++ p02/Form1.cs
@@ -16,5 +16,48 @@ namespace p02
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int posun = Convert.ToInt32(textBox2.Text);
+            string text = textBox1.Text;
+            string sifra = "";
+            foreach (char s in text)
+            {
+                if (s == ' ')
+                {
+                    sifra += " ";
+                }
+                else
+                {
+                    int si = (int)s;
+                    if (si >= 65 && si <= 90)
+                    {
+                        if ((si + posun) >= 89)
+                        {
+                            si = 64 + ((si + posun) - 90);
+                        }
+                        else
+                        {
+                            si = si + posun;
+                        }
+                    }
+                    else if (si >= 97 && si <= 122)
+                    {
+                        if ((si + posun) >= 121)
+                        {
+                            si = 95 + ((si + posun) - 121);
+                        }
+                        else
+                        {
+                            si = si + posun;
+                        }
+                    }
+                    sifra += (char)si;
+                }
+                
+            }
+            MessageBox.Show(sifra);
+        }
     }
 }
