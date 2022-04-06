ba4bd7c308244a644cdf51a0b90e5782d332fec8
novotnyr.05 <novotnyr.05@A222-16>
2022-04-06 09:38:13 +0200

    p05 zatim

---
+++ p05/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            int pocets = 1;
+            if (text == "")
+            {
+                pocets = 0;
+            }
+            else
+            foreach (char s in text)
+            {
+                if ((int)s == 32)
+                {
+                    pocets++;
+                }
+            }
+            MessageBox.Show("pocet slov v textu je " + pocets);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = textBox2.Text;
+            int pocets = 0;
+            int prevBut = 0;
+            bool byl = false;
+            foreach (char s in text)
+            {
+                if (!byl) pocets = 1;byl = true;
+                if ((int)s == 32)
+                {
+                    if (prevBut != 32) pocets++;
+
+                }
+                prevBut = (int)s;
+            }
+            MessageBox.Show("pocet slov v textu je " + pocets);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;//   ahoj   zdar  
+            text = text.Trim();//ahoj   zdar
+            string[] slova = text.Split(' ');
+
+            MessageBox.Show("pocet slov v textu je " + slova.Length);
+        }
+    }
+}
