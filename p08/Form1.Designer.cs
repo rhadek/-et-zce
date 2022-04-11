dfd314bc38818f8df8f176fa3a2c34d01bfc9f0e
novotnyr.05 <novotnyr.05@A222-16>
2022-04-11 09:10:25 +0200

    p08

--- p08/Form1.Designer.cs
+++ p08/Form1.Designer.cs
@@ -36,6 +36,8 @@
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // radioButton1
@@ -43,10 +45,10 @@
             this.radioButton1.AutoSize = true;
             this.radioButton1.Location = new System.Drawing.Point(240, 56);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.Size = new System.Drawing.Size(52, 17);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.Text = "černa";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // checkBox1
@@ -54,14 +56,14 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(144, 58);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);
             this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "pocet slova";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(331, 58);
+            this.button1.Location = new System.Drawing.Point(346, 59);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(87, 104);
             this.button1.TabIndex = 2;
@@ -81,9 +83,9 @@
             this.checkBox2.AutoSize = true;
             this.checkBox2.Location = new System.Drawing.Point(144, 81);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.Size = new System.Drawing.Size(82, 17);
             this.checkBox2.TabIndex = 4;
-            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.Text = "nejdel slovo";
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // radioButton2
@@ -91,41 +93,51 @@
             this.radioButton2.AutoSize = true;
             this.radioButton2.Location = new System.Drawing.Point(240, 81);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.Size = new System.Drawing.Size(64, 17);
             this.radioButton2.TabIndex = 5;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.Text = "červena";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(240, 104);
+            this.radioButton3.Location = new System.Drawing.Point(15, 30);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.Size = new System.Drawing.Size(37, 17);
             this.radioButton3.TabIndex = 6;
             this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.Text = "12";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(240, 127);
+            this.radioButton4.Location = new System.Drawing.Point(15, 53);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.Size = new System.Drawing.Size(37, 17);
             this.radioButton4.TabIndex = 7;
             this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.Text = "20";
             this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Location = new System.Drawing.Point(221, 116);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(104, 100);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.radioButton4);
-            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.textBox1);
@@ -134,6 +146,8 @@
             this.Controls.Add(this.radioButton1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,6 +163,7 @@
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- p08/Form1.cs
+++ p08/Form1.cs
@@ -20,6 +20,50 @@ namespace p08
         private void button1_Click(object sender, EventArgs e)
         {
             string text = textBox1.Text;
+            string[] slova = text.Split(' ');
+            if (checkBox1.Checked)
+            {
+                MessageBox.Show("pocet slov v textu je " + slova.Length);
+            }
+            int pocetpismen = 0;
+            string maxpis = "";
+            int maxp = 0;
+            foreach (string slova1 in slova)
+            {
+                pocetpismen = 0;
+                foreach (char pismena in slova1)
+                {
+                    pocetpismen++;
+                }
+                if (pocetpismen > maxp)
+                {
+                    maxp = pocetpismen;
+                    maxpis = slova1;
+                }
+            }
+            if (checkBox2.Checked)
+            {
+                MessageBox.Show("nejdelsi slovo v textu ma " + maxp + " pismen a je to slovo: " + maxpis);
+            }
+            if (radioButton1.Checked)
+            {
+                textBox1.ForeColor = Color.Black;
+            }
+            else if (radioButton2.Checked)
+            {
+                textBox1.ForeColor = Color.Red;
+            }
+            Font font = new Font("Microsoft Sans Serif", 12);
+            Font font1 = new Font("Microsoft Sans Serif", 20);
+            if (radioButton3.Checked)
+            {
+                textBox1.Font = font;
+            }
+            else if (radioButton4.Checked)
+            {
+                textBox1.Font = font1;
+            }
+
 
         }
     }
